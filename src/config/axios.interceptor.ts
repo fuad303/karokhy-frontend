08e61e0ce77d1b3e2f56a2073844bfa0da73a676
fuad303 <anwariahmadfawad2@gmail.com>
2025-12-07 20:49:19 +0430

    axios interceptor setup

---
+++ src/config/axios.interceptor.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true,
+});
+
+// REQUEST INTERCEPTOR
+api.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// RESPONSE INTERCEPTOR
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      window.location.href = "/login";
+    }
+    if (error.response.status === 403) {
+      console.log(
+        "Use the custom model to tell the user they don't have access to this part"
+      );
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
