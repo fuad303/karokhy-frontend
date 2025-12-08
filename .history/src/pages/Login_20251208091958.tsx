337eed2e7350b8a02779303ee10420ff6c99f954
Rohullah <rohullah313noori313@gmail.com>
2025-12-08 18:06:39 +0430

    remove .history folder

--- .history/src/pages/Login_20251208091958.tsx
+++
@@ -1,5 +0,0 @@
-const Login = () => {
-  return <div>Here is the login</div>;
-};
-
-export default Login;
--- .history/src/schema/Schema_20251208145411.js
+++
@@ -1,5 +0,0 @@
-export const loginSchema = z.object({
-  username: z.string(),
-  password: z.string().min(6, "رمز باید حداقل 6 کاراکتر باشد"),
-  role: z.enum(["مدیر", "شریک", "حسابدار"]),
-});
