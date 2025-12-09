abb5770f97488389abf4ad76eeb929252cea7a2d
Rohullah <rohullah313noori313@gmail.com>
2025-12-09 12:13:32 +0430

    port checking

--- src/config/axios.interceptor.ts
+++ src/config/axios.interceptor.ts
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
-
 // REQUEST INTERCEPTOR
 api.interceptors.request.use(
   (config) => {
+    console.log("url;", config.baseURL);
     return config;
   },
   (error) => Promise.reject(error)
@@ -18,7 +18,7 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response.status === 401) {
-      window.location.href = '/login';
+      window.location.href = "/login";
     }
     if (error.response.status === 403) {
       console.log(
