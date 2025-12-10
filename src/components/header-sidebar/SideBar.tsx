05fadc6cae93cdcfcec725ed48e27d74a62179a0
Hadi Ebrahimi <hadi.ebrahimi3235@gmail.com>
2025-12-10 11:40:49 +0430

    sidebar is updated

--- src/components/header-sidebar/SideBar.tsx
+++ src/components/header-sidebar/SideBar.tsx
@@ -19,47 +19,24 @@ type MenuItem = {
   id: number;
   label: string;
   link: string;
-  icon?: string;
+  icon: string;
 };
 
 export default function SideBar() {
   const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
 
   const iconMap: Record<string, JSX.Element> = {
-    // Dashboard
     dashboardIcon: <LayoutDashboard size={20} />,
-
-    // Purchases
     purchaseIcon: <ShoppingCart size={20} />,
-
-    // Sales
     salesIcon: <ShoppingBag size={20} />,
-
-    // Expenses
     expensesIcon: <Receipt size={20} />,
-
-    // Users
     usersIcon: <Users size={20} />,
-
-    // Money-related pages
     moneyIcon: <DollarSign size={20} />,
-
-    // Installments / Add user / Agreements
     installmentIcon: <UserPlus size={20} />,
-
-    // News / Messages / Communication
     newspaper: <Newspaper size={20} />,
-
-    // Returns / Back navigation / Return items
     returnIcon: <CornerDownLeft size={20} />,
-
-    // Pre-purchase documents
     prePurchaseIcon: <FileSpreadsheet size={20} />,
-
-    // Pre-sale documents
     preSaleIcon: <FileSpreadsheet size={20} />,
-
-    // Water / Liquid / Chemistry / Filters
     Droplets: <Droplets size={20} />,
   };
 
@@ -67,7 +44,6 @@ export default function SideBar() {
     const fetchMenuItems = async () => {
       const res = await api.get('/api/public/menu-items');
       setMenuItems(res.data.menuItems);
-      console.log(res);
     };
 
     fetchMenuItems();
@@ -76,15 +52,15 @@ export default function SideBar() {
   return (
     <aside className="w-64 bg-primary text-white h-screen fixed right-0 top-0 p-4">
       <ul className="space-y-2">
-        {menuItems.map((item) => (
-          <li key={item.id}>
+        {menuItems.map((menu) => (
+          <li key={menu.id}>
             <NavLink
-              to={item.link}
+              to={menu.link}
               className="flex items-center gap-3 p-2 rounded hover:bg-gray-700"
             >
-              {item.icon && iconMap[item.icon]}
+              {menu.icon && iconMap[menu.icon]}
 
-              <span>{item.label}</span>
+              <span>{menu.label}</span>
             </NavLink>
           </li>
         ))}
